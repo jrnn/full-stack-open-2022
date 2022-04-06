f5a546ffcfcab1e4c58e2ca14748d142b75acf28
Juho Juurinen <juho.juurinen@gmail.com>
2022-04-06 16:54:55 +0300

    exercise 3.1

--- part3/phonebook/src/index.ts
+++ part3/phonebook/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import { persons } from "./persons"
 
 const app = express()
 const port = 3001
+const rootUri = "/api/persons"
+
+app.get(rootUri, (_, response) => {
+  response.json(persons)
+})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
