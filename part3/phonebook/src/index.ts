7a74bc053f7b6d747848b40199776df982eeb2ff
Juho Juurinen <juho.juurinen@gmail.com>
2022-04-06 17:04:20 +0300

    exercise 3.2

--- part3/phonebook/src/index.ts
+++ part3/phonebook/src/index.ts
@@ -5,6 +5,13 @@ const app = express()
 const port = 3001
 const rootUri = "/api/persons"
 
+app.get("/info", (_, response) => {
+  response.send(`<div>
+    <p>Phonebook currently has ${persons.length} contacts.</p>
+    <p>Server time: ${new Date().toISOString()}</p>
+  </div>`)
+})
+
 app.get(rootUri, (_, response) => {
   response.json(persons)
 })
