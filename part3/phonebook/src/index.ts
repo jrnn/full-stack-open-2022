a7043eb08df7bfc0b4d604e5241cdde537b9ee58
Juho Juurinen <juho.juurinen@gmail.com>
2022-04-11 21:48:09 +0300

    optimizing docker image size

--- part3/phonebook/src/index.ts
+++ part3/phonebook/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 import db from "./db"
 import infoRouter from "./routers/info"
 import personRouter from "./routers/persons"
@@ -16,7 +17,7 @@ const ENVIRONMENT = process.env["NODE_ENV"] || "development"
 const app = express()
 
 app.use(cors())
-app.use(express.static("static"))
+app.use(express.static(path.resolve(__dirname, "..", "static")))
 app.use(express.json())
 app.use(requestLogger)
 
