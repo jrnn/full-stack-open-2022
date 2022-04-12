f68add56f02e755ba25b284285ecfbb9e0920489
Juho Juurinen <juho.juurinen@gmail.com>
2022-04-12 16:20:05 +0300

    exercise 4.10

--- part4/blogs/src/controllers/blogs.ts
+++ part4/blogs/src/controllers/blogs.ts
@@ -1,14 +1,18 @@
-import { Router } from "express"
-import { BlogModel } from "../models/blog"
+import { Request, Router } from "express"
+import { Blog, BlogModel } from "../models/blog"
 
 const router = Router()
+
+interface TypedRequest<T> extends Request {
+  body: T
+}
 
 router.get("/", async (_, response) => {
   const blogs = await BlogModel.find({})
   return response.status(200).json(blogs)
 })
 
-router.post("/", async (request, response) => {
+router.post("/", async (request: TypedRequest<Blog>, response) => {
   const blog = await new BlogModel(request.body).save()
   return response.status(201).json(blog)
 })
--- part4/blogs/test/api/blogs.test.ts
+++ part4/blogs/test/api/blogs.test.ts
@@ -1,8 +1,12 @@
 import { api } from "../jest.setup"
-import { BlogDocument, BlogModel } from "../../src/models/blog"
+import { Blog, BlogDocument, BlogModel } from "../../src/models/blog"
 import { blogs as initialBlogs } from "../testblogs"
 
 const rootUri = "/api/blogs"
+
+const getBlogsInDb = async () => {
+  return BlogModel.find({})
+}
 
 beforeEach(async () => {
   await BlogModel.deleteMany({})
@@ -16,9 +20,9 @@ describe(`When GET ${rootUri}`, () => {
   })
   it("Then identifies each blog with 'id' attribute", async () => {
     const blogs = await get()
-    console.log(blogs)
     blogs.map(blog => expect(blog.id).toBeDefined())
   })
+
   const get = async (): Promise<Array<BlogDocument>> => {
     const response = await api
       .get(rootUri)
@@ -26,5 +30,53 @@ describe(`When GET ${rootUri}`, () => {
       .expect("Content-Type", /application\/json/)
 
     return response.body as Array<BlogDocument>
+  }
+})
+
+describe(`When POST ${rootUri}`, () => {
+  const newBlog: Blog = {
+    title: "All About Plumbus",
+    author: "Plumbsy McPlumbusface",
+    url: "http://all.about.plumb.us",
+    likes: 2
+  }
+
+  describe("Given valid blog as request body", () => {
+    it("Then adds a new blog to database", async () => {
+      const blogsBefore = await getBlogsInDb()
+      await post(newBlog)
+      const blogsAfter = await getBlogsInDb()
+      expect(blogsAfter).toHaveLength(blogsBefore.length + 1)
+    })
+    it("Then sets the new blog's 'title' to the given 'title'", async () => {
+      const { title } = await postAndGet(newBlog)
+      expect(title).toEqual(newBlog.title)
+    })
+    it("Then sets the new blog's 'author' to the given 'author'", async () => {
+      const { author } = await postAndGet(newBlog)
+      expect(author).toEqual(newBlog.author)
+    })
+    it("Then sets the new blog's 'url' to the given 'url'", async () => {
+      const { url } = await postAndGet(newBlog)
+      expect(url).toEqual(newBlog.url)
+    })
+    it("Then sets the new blog's 'likes' to the given 'likes'", async () => {
+      const { likes } = await postAndGet(newBlog)
+      expect(likes).toEqual(newBlog.likes)
+    })
+  })
+
+  const post = async (blog: Partial<Blog>): Promise<BlogDocument> => {
+    const response = await api
+      .post(rootUri)
+      .send(blog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/)
+
+    return response.body as BlogDocument
+  }
+  const postAndGet = async (blog: Partial<Blog>): Promise<BlogDocument> => {
+    const { id } = await post(blog)
+    return await BlogModel.findById(id) as BlogDocument
   }
 })
