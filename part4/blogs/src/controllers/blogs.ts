b1acc8fc68ab86b0cbd86ed5637130feffc115bd
Juho Juurinen <juho.juurinen@gmail.com>
2022-04-13 19:56:30 +0300

    exercises 4.22, 4.23

--- part4/blogs/src/controllers/blogs.ts
+++ part4/blogs/src/controllers/blogs.ts
@@ -2,8 +2,8 @@ import { Router } from "express"
 import { BlogRequest, BlogModel } from "../models/blog"
 import { AuthorizationError, NotFoundError, throwsError } from "../errors/errors"
 import { TypedRequest } from "../types"
-import { UserModel } from "../models/user"
-import { decodeToken } from "../utils/security"
+import { UserDocument } from "../models/user"
+import userExtractor from "../middleware/userExtractor"
 
 const router = Router()
 const updateOpts = { new: true, runValidators: true }
@@ -16,14 +16,9 @@ router.get("/", throwsError(async (_, response) => {
   return response.status(200).json(blogs)
 }))
 
-router.post("/", throwsError(async (request: TypedRequest<BlogRequest>, response) => {
-  const { body, token } = request
-  const tokenId = decodeToken(token)
-  const user = await UserModel.findById(tokenId)
-  if (!user) {
-    throw new NotFoundError("no users in database")
-  }
-  const blog = await new BlogModel({ ...body, user: user._id }).save()
+router.post("/", userExtractor, throwsError(async ({ body, user }: TypedRequest<BlogRequest>, response) => {
+  const userId = (user as UserDocument)._id
+  const blog = await new BlogModel({ ...body, user: userId }).save()
   return response.status(201).json(blog)
 }))
 
@@ -36,15 +31,15 @@ router.put("/:id", throwsError(async ({ body, params }, response) => {
   return response.status(200).json(updatedBlog)
 }))
 
-router.delete("/:id", throwsError(async (request: TypedRequest, response) => {
-  const userId = decodeToken(request.token)
-  const { id } = request.params
+router.delete("/:id", userExtractor, throwsError(async ({ params, user }: TypedRequest, response) => {
+  const { id } = params
+  const userId = (user as UserDocument)._id
   const blogToDelete = await BlogModel.findById(id)
 
   if (!blogToDelete) {
     throw new NotFoundError(`no blog found with id '${id}'`)
   }
-  if (blogToDelete.user && userId !== blogToDelete.user.toString()) {
+  if (blogToDelete.user && userId.toString() !== blogToDelete.user.toString()) {
     throw new AuthorizationError()
   }
   await blogToDelete.delete()
