1d2f87af9973dcaa85f8f67e7ec8a536fd1cbedd
Juho Juurinen <juho.juurinen@gmail.com>
2022-04-19 09:41:06 +0300

    exercise 5.20

--- part5/blogs/cypress/integration/blogs.test.ts
+++ part5/blogs/cypress/integration/blogs.test.ts
@@ -3,14 +3,26 @@ const user = {
   name: "Spengeblerb Squrupunts",
   password: "qwerty123"
 }
+const coffeeBlog = {
+  title: "All About Coffee",
+  author: "Beany McBeanface",
+  url: "http://all.about.kovfefe"
+}
 
 describe("Blogs app", function() {
 
   const { name, username, password } = user
 
   beforeEach(function() {
-    cy.request("POST", "http://localhost:3003/api/testing/reset")
-    cy.request("POST", "http://localhost:3003/api/users", user)
+    cy.request({
+      url: "http://localhost:3003/api/testing/reset",
+      method: "POST"
+    })
+    cy.request({
+      url: "http://localhost:3003/api/users",
+      method: "POST",
+      body: user
+    })
     cy.visit("http://localhost:8080")
   })
 
@@ -55,9 +67,10 @@ describe("Blogs app", function() {
   describe("when logged in", function() {
     beforeEach(function () {
       cy.login({ username, password })
+      cy.createBlog(coffeeBlog)
     })
 
-    it("a blog can be created by (and for) the logged-in user", function() {
+    it("a blog can be created", function() {
       cy.contains("Click here to add new blog").click()
       cy.get("#title-input").type("All About Boats")
       cy.get("#author-input").type("Boaty McBoatface")
@@ -78,6 +91,21 @@ describe("Blogs app", function() {
       cy.get("#author-input").should("not.be.visible")
       cy.get("#url-input").should("not.be.visible")
       cy.get("#createBlog-button").should("not.be.visible")
+    })
+
+    it("a blog can be liked", function() {
+      cy.contains(`${coffeeBlog.author}: "${coffeeBlog.title}"`)
+        .parent()
+        .as("existingBlog")
+        .contains("Show details")
+        .click()
+
+      cy.get("@existingBlog").should("contain", "Likes: 0")
+      cy.get("@existingBlog")
+        .contains("Like!")
+        .click()
+
+      cy.get("@existingBlog").should("contain", "Likes: 1")
     })
   })
 })
--- part5/blogs/cypress/support/index.ts
+++ part5/blogs/cypress/support/index.ts
@@ -3,14 +3,32 @@
 declare global {
   namespace Cypress {
     interface Chainable {
+      createBlog(blog: { title: string, author: string, url: string}): Chainable<Element>
       login(credentials: { username: string, password: string }): Chainable<Element>
     }
   }
 }
 
+const userAuthKey = "FSO22_PART5_BLOGS_WEB_CLIENT_USER_AUTH"
+
+Cypress.Commands.add("createBlog", blog => {
+  cy.request({
+    url: "http://localhost:3003/api/blogs",
+    method: "POST",
+    body: blog,
+    headers: {
+      authorization: `bearer ${JSON.parse(localStorage.getItem(userAuthKey)).token}`
+    }
+  })
+})
+
 Cypress.Commands.add("login", credentials => {
-  cy.request("POST", "http://localhost:3003/api/login", credentials)
-    .then(({ body }) => localStorage.setItem("FSO22_PART5_BLOGS_WEB_CLIENT_USER_AUTH", JSON.stringify(body)))
+  cy.request({
+    url: "http://localhost:3003/api/login",
+    method: "POST",
+    body: credentials
+  })
+    .then(({ body }) => localStorage.setItem(userAuthKey, JSON.stringify(body)))
     .then(() => cy.visit("http://localhost:8080"))
 })
 
