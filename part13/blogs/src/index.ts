c9e4c7e95a6f8cf0816769b21cdc2b2592fb8b0f
Juho Juurinen <juho.juurinen@gmail.com>
2022-05-24 17:04:54 +0300

    exercise 13.1

--- part13/blogs/src/index.ts
+++ part13/blogs/src/index.ts
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 
+const MODE = process.env["NODE_ENV"] || "development"
 const PORT = process.env["PORT"] || 3001
 
 const app = express()
@@ -10,5 +11,5 @@ app.use(cors())
 app.get("/api/ping", (_, response) => response.status(200).send("pong"))
 
 app.listen(PORT, () => {
-  console.log(`Now running on port ${PORT}`)
+  console.log(`Now running on port ${PORT} in ${MODE} mode.`)
 })
