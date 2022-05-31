effb7340968d2dbdeabb2f6551170728df0412fa
Juho Juurinen <juho.juurinen@gmail.com>
2022-05-31 09:18:20 +0300

    exercise 13.6

--- part13/blogs/src/routes/blogs.ts
+++ part13/blogs/src/routes/blogs.ts
@@ -1,12 +1,27 @@
-import { Router } from "express"
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+import { Request, RequestHandler, Router } from "express"
 import { Blog } from "../models"
+
+interface RequestWithBlog<T = unknown> extends Request<{ id?: string }> {
+  body: T
+  blog?: Blog
+}
+
+const blogFinder: RequestHandler = async (request: RequestWithBlog, response, next) => {
+  const blog = await Blog.findByPk(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
+  }
+  request.blog = blog
+  return next()
+}
 
 export const blogsRouter = Router()
 
 blogsRouter.get("/", async (_, response) => {
   const blogs = await Blog.findAll()
-  const blogsJson = blogs.map(blog => blog.toJSON())
-  return response.status(200).json(blogsJson)
+  return response.status(200).json(blogs)
 })
 
 blogsRouter.post("/", async ({ body }, response) => {
@@ -18,11 +33,18 @@ blogsRouter.post("/", async ({ body }, response) => {
   }
 })
 
-blogsRouter.delete("/:id", async ({ params }, response) => {
-  const blog = await Blog.findByPk(params.id)
-  if (!blog) {
-    return response.status(404).end()
+blogsRouter.put("/:id", blogFinder, async ({ blog, body }: RequestWithBlog<{ likes: number }>, response) => {
+  try {
+    const { likes } = body
+    blog!.likes = likes
+    await blog!.save()
+    return response.status(200).json(blog)
+  } catch (error) {
+    return response.status(400).json({ error })
   }
-  await blog.destroy()
+})
+
+blogsRouter.delete("/:id", blogFinder, async ({ blog }: RequestWithBlog, response) => {
+  await blog!.destroy()
   return response.status(204).end()
 })
