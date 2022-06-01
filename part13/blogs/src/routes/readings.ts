d66808a6df7f5a2642f6f0c147585410a755b6f0
Juho Juurinen <juho.juurinen@gmail.com>
2022-06-01 21:24:50 +0300

    exercise 13.22

--- part13/blogs/src/routes/readings.ts
+++ part13/blogs/src/routes/readings.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import { NotFoundError, throwsError } from "../errors"
+import { userExtractor } from "../middleware"
 import { Blog, Reading, User } from "../models"
 import { TypedRequest } from "../types"
 
@@ -22,4 +23,22 @@ readingsRouter.post("/", throwsError(async (request: TypedRequest<ReadingDto>, r
   }
   const reading = await Reading.create({ blogId, userId })
   response.status(201).json(reading)
+}))
+
+readingsRouter.put("/:blogId", userExtractor, throwsError(async (request: TypedRequest<{ hasBeenRead: boolean }>, response) => {
+  const user = request.user as User
+  const blogId = request.params["blogId"] || "-1"
+  const reading = await Reading.findOne({
+    where: {
+      blogId,
+      userId: user.id
+    }
+  })
+  if (!reading) {
+    throw new NotFoundError(`no reading found for the current user for blog '${blogId}'`)
+  }
+  const { hasBeenRead } = request.body
+  reading.hasBeenRead = hasBeenRead
+  await reading.save()
+  return response.status(200).json(reading)
 }))
